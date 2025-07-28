d4c320b5ee53903d4fc2adf677b6a150f6a91a2c
John R Patek Sr <johnpatek2@gmail.com>
2025-07-28 17:17:50 -0500

    trying to fix share directory errors

--- ports/sigfn/portfile.cmake
+++ ports/sigfn/portfile.cmake
@@ -13,6 +13,13 @@ vcpkg_cmake_configure(
 vcpkg_cmake_build()
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
