398acf92275dc1930362a23960debb0e4a6e7f95
John R Patek Sr <johnpatek2@gmail.com>
2025-07-28 17:12:43 -0500

    trying without pkgconfig

--- ports/sigfn/portfile.cmake
+++ ports/sigfn/portfile.cmake
@@ -14,10 +14,6 @@ vcpkg_cmake_build()
 
 vcpkg_cmake_install()
 
-vcpkg_fixup_pkgconfig()
-    
-vcpkg_cmake_config_fixup()
-
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
